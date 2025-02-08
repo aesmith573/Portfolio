ea99a5846cd00efd1726aa1f18f327c7086522fc
aesmith573 <aesmith.573@gmail.com>
2025-02-08 04:05:39 -0600

    Switch navbar to Image template with React logo placeholder.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,12 @@
 
 function Navbar() {
   return(<>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
-        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
-        aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About Me</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Projects</a>
-            </li>
-          </ul>
-        </div>
+      <nav class="navbar bg-body-tertiary">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="src\assets\react.svg" 
+          alt="Bootstrap" width="30" height="24"/>
+        </a>
       </div>
     </nav>
   </>)
