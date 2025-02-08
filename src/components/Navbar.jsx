2985f4d38bf9759146a336cabeb3dc652abc9ff2
aesmith573 <aesmith.573@gmail.com>
2025-02-08 03:58:22 -0600

    Simplify navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ function Navbar() {
   return(<>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
         data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
         aria-label="Toggle navigation">
@@ -15,13 +14,10 @@ function Navbar() {
               <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">About Me</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a className="nav-link" href="#">Projects</a>
             </li>
           </ul>
         </div>
