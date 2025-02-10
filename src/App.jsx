c55b6a3f7cee75735de1f4c7419d0a35a4463019
aesmith573 <aesmith.573@gmail.com>
2025-02-10 04:49:26 -0600

    Create Project card component with 2 implementations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 import Project from './components/Project.jsx';
 import Navbar from './components/Navbar.jsx';
 import AboutMe from './components/AboutMe.jsx';
+import transmngment from './assets/projects/transmngment.png';
+import PutMeOn from './assets/projects/PutMeOn.png';
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <AboutMe/>
-      <Project/>
+      <Navbar />
+      <AboutMe />
+      <div className='container'>
+        <div className='row justify-content-evenly'>
+          <div className='col-md-6 col-lg-4'>
+          {/* Financial App */}
+            <Project
+              cardImage={transmngment}
+              cardTitle={"Financial Web Application"}
+              cardDescription= {
+                "Transaction management tool made with C#, .NET, SQL, JavaScript, " +
+                "HTML/CSS. It has features like user login/registration as well as " +
+                "full CRUD functionality. Fully hosted and deployed on Azure!"
+              }
+              cardLink="https://financialapp-demo.azurewebsites.net/"
+            />
+          </div>
+          {/* PutMeOn */}
+          <div className='col-md-6 col-lg-4'>
+            <Project
+              cardImage={PutMeOn}
+              cardTitle={"PutMeOn"}
+              cardDescription= {
+                "Worked with a team of developers for my senior capstone project." +
+                " I worked on UI/UX as well as media design for the project."
+              }
+              cardLink="https://github.com/corbin-poteet/PutMeOn"
+            />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,20 @@
 
-function Project() {
-  return(<>
-    <p>This is one of my projects.</p>
-  </>);
+
+function Project(props) {
+  return(
+    <div className="card m-4 p-1" style={{ width: "22rem" }} >
+      <img src={props.cardImage} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">{props.cardTitle}</h5>
+        <p className="card-text">{props.cardDescription}</p>
+        <a 
+          href={props.cardLink} 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-primary">Check it out!</a>
+      </div>
+    </div>
+  );
 }
 
 export default Project;
