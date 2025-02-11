3e2b46ee01ca8f874df0bed956a31666e7a77143
aesmith573 <aesmith.573@gmail.com>
2025-02-11 05:35:44 -0600

    Remove button and style cards.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,24 +4,28 @@ function Project(props) {
   const [isHovered, setIsHovered] = useState(false); 
 
   return(
-    <div className="card m-4 p-1" style={{ width: "22rem" }} 
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+    <a
+      href={props.cardLink} 
+      target="_blank"
+      rel="noopener noreferrer"
+      className='card-container'
     >
-      <img 
-        src={isHovered && props.cardGif ? props.cardGif : props.cardImage} 
-        className="card-img-top" 
-        alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">{props.cardTitle}</h5>
-        <p className="card-text">{props.cardDescription}</p>
-        <a 
-          href={props.cardLink} 
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn btn-primary">Check it out!</a>
+      <div 
+        className="card m-4 p-1" style={{ width: "22rem" }} 
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img 
+          src={isHovered && props.cardGif ? props.cardGif : props.cardImage} 
+          className="card-img-top" 
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{props.cardTitle}</h5>
+          <p className="card-text">{props.cardDescription}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
